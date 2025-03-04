dc0b8fee1a3f28ab3f8e1d7ad3c0024d4830dc6f
Patricia <nekepatri@gmail.com>
2025-03-04 11:59:35 +0300

    Use templates to build flask application

--- server.py
+++ server.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+
+# Dummy blog posts
+posts = [
+    {"title": "First Blog Post", "content": "This is my first blog post!", "author": "Jane Doe"},
+    {"title": "Second Blog Post", "content": "This is another blog post!", "author": "John Doe"}
+]
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("index.html", posts=posts)
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+    return render_template('about.html')
 
+# Dynamic routes
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+    # Logic to view a given post by post_id
+    if 0 <= post_id < len(posts):
+        return render_template('post.html', post=posts[post_id])
+    
 if __name__ == '__main__':
     app.run()
